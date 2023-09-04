f94f09b138e20775ee112dd0f1e326740083d85a
Brayan Vargas <brayanvargasbarajas@gmail.com>
2023-09-04 16:06:54 -0500

    Change variables to env variables

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { config } = require('./config/config');
 const { errorHandler, logError, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = config.host;
+const port = config.port;
+const host = config.host;
 const whitelist = ['http://localhost:3000/', 'http://localhost:*'];
 
 const options = {
@@ -27,4 +28,6 @@ app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port);
+app.listen(port, host, () => {
+  console.log(`Listen on port:${port}, host:${host}`);
+});
